ace94a4c3f9ef225a8d732aaeb4b4f42e618455c
bbyojjok <bbyojjok@gmail.com>
2022-08-30 16:55:48 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const API_KEY = '08eff85bbf86ace4fc529a16066a71c2';
+
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path*',
+        destination: '/new-blog/:path*',
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <>
       <Seo title="Home" />
-      <div>Home</div>
+      <div>
+        Home
+        {!results && <h4>loading...</h4>}
+        <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {results?.map((movie) => (
+            <li style={{ width: '50%' }} key={movie.id}>
+              <img
+                style={{ width: '150px' }}
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              />
+              <div>{movie.original_title}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch('http://localhost:3000/api/movies')
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
